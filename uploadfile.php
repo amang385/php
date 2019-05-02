23dda536032af0a6e6f81acf9019c413f2c4fe3b
amang <yc.std.dev@gmail.com>
2019-05-02 13:39:17 +0700

    Update uploadfile.php

--- uploadfile.php
+++ uploadfile.php
@@ -16,3 +16,24 @@ $uploads_dir = '/photo/';
      $pname = $name . $increment . '.' . $extension;
    }
    move_uploaded_file($tname, $uploads_dir.'/'.$pname);
+  // ----------------------------------------------------
+
+         $filepart = $_FILES["Category_img"]["tmp_name"];
+         $filename = $_FILES["Category_img"]["name"];
+         $filesize = $_FILES["Category_img"]["size"];
+
+        if($filepart != ""){
+          if($filesize <= 1000000){
+            $fileextension = end(explode(".",$filename));
+            $filename = "Pro".time().".".$fileextension;
+            move_uploaded_file($filepart,"../img/category/".$filename);
+            if($_POST["DB_Category_img"] != ""){
+                unlink("../img/category/".$_POST["DB_Category_img"]);
+            }
+            $Category_img = $filename;
+          }else if($filesize >= 3000000){
+            echo("<script> alert('ไม่สามารถอัพไฟล์ได้เกิน 3mb');</script>");
+          }
+        }else{
+            $Category_img = $_POST["DB_Category_img"];
+        }
